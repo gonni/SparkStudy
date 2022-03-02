02defef3eef1756721422bd342a3262c226f0a7d
YoungGon Kim <ygkim@YoungGonui-MacBookAir.local>
2022-03-03 00:23:22 +0900

    daily commit

--- build.sbt
+++ build.sbt
@@ -5,6 +5,9 @@ version := "0.1"
 ThisBuild / scalaVersion := "2.12.10"
 
 val sparkVersion = "3.1.2"
+val akkaVersion = "2.5.26"
+val akkaHttpVersion = "10.1.11"
+
 resolvers += "jitpack" at "https://jitpack.io"
 
 
@@ -17,5 +20,13 @@ lazy val root = (project in file("."))
       "org.apache.spark" %% "spark-mllib" % sparkVersion,
       "mysql" % "mysql-connector-java" % "5.1.44",
       "com.github.shin285" % "KOMORAN" % "3.3.4",
+      //"com.typesafe.akka" %% "akka-actor" % akkaVersion,
+      //"com.typesafe.akka" %% "akka-testkit" % akkaVersion,
+      // akka streams
+      "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+      // akka http
+      "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+      "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
+      "com.influxdb" % "influxdb-client-scala_2.12" % "4.3.0"
     )
   )
---
+++ src/main/scala/com/yg/tsdb/Influx18Sample.scala
@@ -0,0 +1,55 @@
+package com.yg.tsdb
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMethods, HttpRequest, HttpResponse}
+import akka.stream.ActorMaterializer
+
+import java.net.URLEncoder
+import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
+
+object Influx18Sample {
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+  import system.dispatcher
+
+  val source =
+    """
+      |object SimpleApp {
+      |  val aField = 2
+      |
+      |  def aMethod(x: Int) = x + 1
+      |
+      |  def main(args: Array[String]) = {
+      |    println(aMethod(aField))
+      |  }
+      |}
+    """.stripMargin
+
+  val request = HttpRequest(
+    method = HttpMethods.POST,
+    uri = "http://markup.su/api/highlighter",
+    entity = HttpEntity(
+      ContentTypes.`application/x-www-form-urlencoded`,
+      s"source=${URLEncoder.encode(source.trim, "UTF-8")}&language=Scala&theme=Sunburst"
+    )
+  )
+
+  def sendRequest() = {
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(request)
+    val entityFuture: Future[HttpEntity.Strict] = responseFuture.flatMap(_.entity.toStrict(2 seconds))
+    entityFuture.map(entity => entity.data.utf8String)
+  }
+
+  def simpleRequest() = {
+    val responseFuture = Http().singleRequest(request)
+    responseFuture.flatMap(_.entity.toStrict(2 seconds)).map(_.data.utf8String).foreach(println)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("Active")
+
+    sendRequest().foreach(println)
+  }
+}
